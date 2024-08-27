d61db5c55f8eb06067c4802aeedec8879a6052ea
Antonio Bermudez <mokuadb@gmail.com>
2024-08-27 15:43:10 +0200

    actualizacion

---
+++ 03_strings.py
@@ -0,0 +1,19 @@
+letter = "A"
+print(letter)
+print(len(letter))
+
+gretting = "Hello World"
+print(gretting)
+print(len(gretting))
+
+
+
+
+
+
+
+#  Formateo
+
+name, surname = "Antonio", "Bermudez"
+
+print("Mi nombre es %s %s", name, surname)
